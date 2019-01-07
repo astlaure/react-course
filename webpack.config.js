a68824ad0cb88cc8213ef5a4dbb2b1fbb09d7902
Alexandre St-Laurent <alexandre.st-laurent@intact.net>
2019-01-07 11:29:16 -0500

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,64 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: process.env.final ? "./final/index.jsx" : "./src/index.jsx",
-  output: {
-    filename: "[name].bundle.js",
-    path: __dirname + process.env.final ? "/fdist" : '/dist',
-  },
-  resolve: {
-    extensions: ['.jsx', '.js']
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
-      { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader'] },
-      { test:/\.(png|jpg|svg)$/, loader: 'file-loader'}
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: process.env.final ? "./final/index.html" : './src/index.html',
-    })
-  ],
-  devtool: 'source-map',
-  devServer: {
-    historyApiFallback: true,
-    before(app) {
-      app.get('/pizzas', (req, res) => {
-        res.json([
-          {
-            id: 1,
-            name: 'Cheese',
-            price: '10.99',
-            ingredients: [
-              'cheese',
-            ]
-          },
-          {
-            id: 2,
-            name: 'Pepperoni',
-            price: '12.99',
-            ingredients: [
-              'cheese',
-              'pepperoni',
-            ]
-          },
-          {
-            id: 3,
-            name: 'Hawaiian',
-            price: '15.99',
-            ingredients: [
-              'cheese',
-              'ham',
-              'pineapple'
-            ]
-          }
-        ])
+module.exports = env => {
+  return {
+    entry: env.final ? "./final/index.jsx" : "./src/index.jsx",
+    output: {
+      filename: "[name].bundle.js",
+      path: __dirname + env.final ? "/fdist" : '/dist',
+    },
+    resolve: {
+      extensions: ['.jsx', '.js']
+    },
+    mode: "development",
+    module: {
+      rules: [
+        {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
+        { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader'] },
+        { test:/\.(png|jpg|svg)$/, loader: 'file-loader'}
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: env.final ? "./final/index.html" : './src/index.html',
       })
+    ],
+    devtool: 'source-map',
+    devServer: {
+      historyApiFallback: true,
+      before(app) {
+        app.get('/pizzas', (req, res) => {
+          res.json([
+            {
+              id: 1,
+              name: 'Cheese',
+              price: '10.99',
+              ingredients: [
+                'cheese',
+              ]
+            },
+            {
+              id: 2,
+              name: 'Pepperoni',
+              price: '12.99',
+              ingredients: [
+                'cheese',
+                'pepperoni',
+              ]
+            },
+            {
+              id: 3,
+              name: 'Hawaiian',
+              price: '15.99',
+              ingredients: [
+                'cheese',
+                'ham',
+                'pineapple'
+              ]
+            }
+          ])
+        })
+      }
     }
   }
 };
