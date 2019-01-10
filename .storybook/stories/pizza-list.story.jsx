9f334cd604c106c3766fdd19c534040dd1ef4d59
Alexandre St-Laurent <alexandre.st-laurent@intact.net>
2019-01-10 09:25:31 -0500

    storybook addons

--- .storybook/stories/pizza-list.story.jsx
+++ .storybook/stories/pizza-list.story.jsx
@@ -1,6 +1,8 @@
 import "../../node_modules/bootstrap/scss/bootstrap.scss";
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, text } from '@storybook/addon-knobs';
+import { withNotes } from '@storybook/addon-notes';
 import { PizzaList } from '../../final/app/pizzas/pizza-list/pizza-list';
 
 import { BrowserRouter } from 'react-router-dom';
@@ -40,17 +42,23 @@ const Pizzas = [
 ];
 
 storiesOf('PizzaList', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withNotes)
   .add('with 1 pizza', () => (
     <BrowserRouter>
       <Provider store={store}>
-        <PizzaList pizzas={Pizzas.slice(0, 1)} filter={undefined} />
+        <PizzaList pizzas={Pizzas.slice(0, 1)} filter={text('filter', undefined)} />
       </Provider>
     </BrowserRouter>
   ))
-  .add('with 3 pizzas', () => (
-    <BrowserRouter>
-      <Provider store={store}>
-        <PizzaList pizzas={Pizzas} filter={undefined} />
-      </Provider>
-    </BrowserRouter>
-  ));
+  .add(
+    'with 3 pizzas',
+    () => (
+      <BrowserRouter>
+        <Provider store={store}>
+          <PizzaList pizzas={Pizzas} filter={undefined} />
+        </Provider>
+      </BrowserRouter>
+    ),
+    { notes: 'Like in the running app. TODO knobs' }
+  );
